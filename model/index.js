b500e54288e53dcd755e1c88e2a80774fda65af4
GG <gerardstjosol@users.noreply.github.com>
2021-01-06 01:48:50 +0800

    moved images, icons, css, and html to static folder
    can now open website through localhost when index.js is run

--- model/index.js
+++ model/index.js
@@ -1,9 +1,36 @@
 const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
 const app = express();
 const port = 9090;
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+// parse application/json
+app.use(bodyParser.json())
+// set static folder 
+app.use(express.static(path.join(__dirname, '/../static')))
+
+//default route
 app.get('/', function(req,res){
-    res.send('Hello World!');
+    res.sendFile(__dirname+"/../static/index.html");
+})
+
+//nike page
+app.get('/nike.html', function(req,res){
+    res.sendFile(__dirname+"/../static/nike.html");
+})
+
+//login page
+app.get('/login.html', function(req,res){
+    res.sendFile(__dirname+"/../static/login.html");
+})
+
+//product page
+app.get('/product.html', function(req,res){
+    res.sendFile(__dirname+"/../static/product.html");
 })
 
 app.listen(port, function(){
